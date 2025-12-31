0a16062d71f33cfa3b6946ef3179de423ce70539
Apnimaa <patelkinjal1212@gmail.com>
2025-12-31 06:17:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ bot = Client(
         api_id=27433400,
         api_hash="1a286620de5ffe0a7d9b57e604293555",
         sleep_threshold=120,
-        plugins="plugins",
+        plugins=PLUGINS,
         workers=8
 )
 
